c615b5cec0137d08fbdee6e1fb1e855129fde9a9
Anikesh <hi@anikesh.dev>
2025-04-01 23:35:08 +0530

    Update

--- sql.php
+++ sql.php
@@ -101,31 +101,30 @@ if ($conn->query($sql) === FALSE) {
     die("Error creating user table: " . $conn->error);
 }
 
-$sql = "CREATE TABLE IF NOT EXISTS `discount` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `desc` text,
-  `discount_percent` decimal(10,2) NOT NULL,
-  `active` boolean NOT NULL DEFAULT TRUE,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-)";
+// $sql = "CREATE TABLE IF NOT EXISTS `discount` (
+//   `id` int NOT NULL AUTO_INCREMENT,
+//   `name` varchar(255) NOT NULL,
+//   `desc` text,
+//   `discount_percent` decimal(10,2) NOT NULL,
+//   `active` boolean NOT NULL DEFAULT TRUE,
+//   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+//   `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+//   `deleted_at` timestamp NULL DEFAULT NULL,
+//   PRIMARY KEY (`id`)
+// )";
 
-if ($conn->query($sql) === FALSE) {
-    die("Error creating user table: " . $conn->error);
-}
+// if ($conn->query($sql) === FALSE) {
+//     die("Error creating user table: " . $conn->error);
+// }
 
 $sql = "CREATE TABLE IF NOT EXISTS `product` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `desc` text,
   `SKU` varchar(50) NOT NULL,
-  `category_id` int NOT NULL,
-  `inventory_id` int NOT NULL,
+  `category` varchar(255) NOT NULL,
+  -- `inventory_id` int NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `discount_id` int,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL,
